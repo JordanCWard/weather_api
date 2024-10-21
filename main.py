1314c118f5d0e90e46b5ca6e973099f14aa8e756
Jordan Ward <jordancward@gmail.com>
2024-10-21 15:02:03 -0600

    Add dict to website

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # Import flask to use multiple HTML web pages
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
 # the next line is connected to home function because of the @ symbol
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# Special syntax around station and date
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-# Adding debug=True allows us to see errors on the web page
-app.run(debug=True)
+# Only runs the website if this script is run directly from this file
+if __name__ == "__main__":
+    # Adding debug=True allows us to see errors on the web page
+    app.run(debug=True)
