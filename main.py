9efd6059267397f49fb431d947faefb5f74aa695
Jordan Ward <jordancward@gmail.com>
2024-10-28 08:46:21 -0600

    Return temperature for any station and date using Pandas

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Import flask to use multiple HTML web pages
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
-
 
 # the next line is connected to home function because of the @ symbol
 @app.route("/")
@@ -13,7 +13,15 @@ def home():
 # Special syntax around station and date
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+
+    # zfill adds zeros on the left till we have a 6-digit number
+    filename = "weather_station_data/TG_STAID" + str(station).zfill(6) + ".txt"
+
+    # Skip 20 rows because tables start at line 20 in each file
+    weather_data = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = weather_data.loc[weather_data['    DATE'] == date]['   TG'].squeeze() / 10
+
+
     return {"station": station,
             "date": date,
             "temperature": temperature}
